bb0bfcf5494aa4f7c1ef21a46fd39aeb10625b71
R3dL4w <wbuchholtzer@gmail.com>
2022-06-12 19:27:58 +0300

    Improved README file

--- HPS_tuner.py
+++ HPS_tuner.py
@@ -3,7 +3,7 @@
 has a sound trigger (level threshold trigger), applies a high pass filter with a cutoff frequency of 50 Hz (the AC frequency in
 Europe) and uses the Harmonic Product Spectrum (HPS) for robustness and efficiency. In the last part, it
 computes the closest musical note to the input signal frequency, calculates the the 'Target Frequency' i.e
-the actual frequency of the musical note and and it prints in console all of this, together with the obtained
+the actual frequency of the musical note and and it prints in the console together with the obtained
 frequency of the input signal."""
 
 #Author: Walter Buchholtzer
